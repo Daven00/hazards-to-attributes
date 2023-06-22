a4d11c51b3b4821361a473506b790e4343f5f057
marco-rasori <marco.rasori@gmail.com>
2023-06-22 13:19:54 +0200

    Get the attributeId from the ontology; Refactor code with more methods

--- src/main/java/eu/sifishome/OntologyReader.java
+++ src/main/java/eu/sifishome/OntologyReader.java
@@ -36,11 +36,13 @@ public class OntologyReader {
             System.out.println("Ontology Version: " + ontologyVersion);
             System.out.println();
 
-            // Define the hazard class and riskScore property
+            // Define the hazard class, the riskScore property, and the attributeId property
             IRI hazardIRI = IRI.create("https://purl.org/sifis/hazards#Hazard");
             OWLClass hazardClass = dataFactory.getOWLClass(hazardIRI);
             IRI riskScoreIRI = IRI.create("https://purl.org/sifis/hazards#riskScore");
             OWLDataProperty riskScoreProperty = dataFactory.getOWLDataProperty(riskScoreIRI);
+            IRI attributeIdIRI = IRI.create("https://purl.org/sifis/hazards#attributeId");
+            OWLDataProperty attributeIdProperty = dataFactory.getOWLDataProperty(attributeIdIRI);
 
             // Create an instance of OWLReasoner to perform reasoning
             OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
@@ -62,68 +64,16 @@ public class OntologyReader {
             for (OWLNamedIndividual individual : individuals) {
                 if (reasoner.getTypes(individual, false).containsEntity(hazardClass)) {
                     // The individual is of type Hazard
-                    String camelCaseShortForm = individual.getIRI().getShortForm();
-                    String shortForm = convertToKebabCase(camelCaseShortForm);
                     System.out.println();
-                    System.out.println("Individual: " + shortForm);
+                    System.out.println("Individual: " + individual.getIRI().getShortForm());
                     System.out.println();
 
-                    System.out.println("Request:");
-                    System.out.println("<Attribute AttributeId=\"eu:sifis-home:1.0:hazard:" + shortForm +
-                            "\" IncludeInResult=\"false\">");
+                    String attributeId = getAttributeId(ontology, individual, attributeIdProperty);
+                    boolean hasRiskScore = hasRiskScore(ontology, individual, riskScoreProperty);
 
-                    boolean hasRiskScore = false;
-                    for (OWLAxiom axiom : ontology.getAxioms(individual)) {
-                        if (axiom instanceof OWLDataPropertyAssertionAxiom) {
-                            OWLDataPropertyAssertionAxiom dataPropertyAxiom = (OWLDataPropertyAssertionAxiom) axiom;
-                            if (dataPropertyAxiom.getProperty().equals(riskScoreProperty)) {
-                                // The individual has the riskScore property
-                                hasRiskScore = true;
-                                break;
-                            }
-                        }
-                    }
+                    printAttributeForRequest(attributeId, hasRiskScore);
+                    printAttributeForPolicy(attributeId, hasRiskScore);
 
-                    if (hasRiskScore) {
-                        System.out.println("  <AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#integer\">1..10</AttributeValue>");
-                    } else {
-                        System.out.println("  <AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#boolean\">true</AttributeValue>");
-                    }
-                    System.out.println("</Attribute>");
-
-                    System.out.println();
-                    System.out.println("Policy:");
-                    System.out.println("<Apply");
-                    if (hasRiskScore) {
-                        System.out.println("  FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:integer-greater-than\">");
-                    } else {
-                        System.out.println("  FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:boolean-equal\">");
-                    }
-                    System.out.println("  <Apply");
-                    if (hasRiskScore) {
-                        System.out.println("    FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:integer-one-and-only\">");
-                    } else {
-                        System.out.println("    FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:boolean-one-and-only\">");
-                    }
-                    System.out.println("    <AttributeDesignator");
-                    System.out.println("      AttributeId=\"eu:sifis-home:1.0:hazard:" + shortForm + "\"");
-                    System.out.println("      Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:environment\"");
-                    if (hasRiskScore) {
-                        System.out.println("      DataType=\"http://www.w3.org/2001/XMLSchema#integer\"");
-                    } else {
-                        System.out.println("      DataType=\"http://www.w3.org/2001/XMLSchema#boolean\"");
-                    }
-                    System.out.println("      MustBePresent=\"true\">");
-                    System.out.println("    </AttributeDesignator>");
-                    System.out.println("  </Apply>");
-                    System.out.println("  <AttributeValue");
-                    if (hasRiskScore) {
-                        System.out.println("    DataType=\"http://www.w3.org/2001/XMLSchema#integer\">1..10</AttributeValue>");
-                    } else {
-                        System.out.println("    DataType=\"http://www.w3.org/2001/XMLSchema#boolean\">true</AttributeValue>");
-                    }
-                    System.out.println("</Apply>");
-                    System.out.println();
                     System.out.println("----------------------------");
                 }
             }
@@ -135,13 +85,78 @@ public class OntologyReader {
         }
     }
 
-    public static String convertToKebabCase(String input) {
-        String regex = "([a-z0-9])([A-Z])";
-        String replacement = "$1-$2";
-        String kebabCaseString = input.replaceAll(regex, replacement).toLowerCase();
-        if (Character.isUpperCase(input.charAt(0))) {
-            kebabCaseString = Character.toLowerCase(kebabCaseString.charAt(0)) + kebabCaseString.substring(1);
+    public static String getAttributeId(OWLOntology ontology, OWLNamedIndividual hazard, OWLDataProperty attributeIdProperty) {
+        // Get the value of the attributeId data property
+        for (OWLAxiom axiom : ontology.getAxioms(hazard)) {
+            if (axiom instanceof OWLDataPropertyAssertionAxiom) {
+                OWLDataPropertyAssertionAxiom dataPropertyAxiom = (OWLDataPropertyAssertionAxiom) axiom;
+                if (dataPropertyAxiom.getProperty().equals(attributeIdProperty)) {
+                    OWLLiteral attributeIdValue = dataPropertyAxiom.getObject();
+                    return attributeIdValue.getLiteral();
+                }
+            }
         }
-        return kebabCaseString;
+        return null;
+    }
+
+    public static boolean hasRiskScore(OWLOntology ontology, OWLNamedIndividual hazard, OWLDataProperty riskScoreProperty) {
+        for (OWLAxiom axiom : ontology.getAxioms(hazard)) {
+            if (axiom instanceof OWLDataPropertyAssertionAxiom) {
+                OWLDataPropertyAssertionAxiom dataPropertyAxiom = (OWLDataPropertyAssertionAxiom) axiom;
+                if (dataPropertyAxiom.getProperty().equals(riskScoreProperty)) {
+                    // The hazard has the riskScore property
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printAttributeForRequest(String attributeId, boolean hasRiskScore) {
+        System.out.println("Request:");
+        System.out.println("<Attribute AttributeId=\"" + attributeId + "\" IncludeInResult=\"false\">");
+
+        if (hasRiskScore) {
+            System.out.println("  <AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#integer\">1..10</AttributeValue>");
+        } else {
+            System.out.println("  <AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#boolean\">true</AttributeValue>");
+        }
+        System.out.println("</Attribute>");
+        System.out.println();
+    }
+
+    public static void printAttributeForPolicy(String attributeId, boolean hasRiskScore) {
+        System.out.println("Policy:");
+        System.out.println("<Apply");
+        if (hasRiskScore) {
+            System.out.println("  FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:integer-greater-than\">");
+        } else {
+            System.out.println("  FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:boolean-equal\">");
+        }
+        System.out.println("  <Apply");
+        if (hasRiskScore) {
+            System.out.println("    FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:integer-one-and-only\">");
+        } else {
+            System.out.println("    FunctionId=\"urn:oasis:names:tc:xacml:1.0:function:boolean-one-and-only\">");
+        }
+        System.out.println("    <AttributeDesignator");
+        System.out.println("      AttributeId=\""+ attributeId + "\"");
+        System.out.println("      Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:environment\"");
+        if (hasRiskScore) {
+            System.out.println("      DataType=\"http://www.w3.org/2001/XMLSchema#integer\"");
+        } else {
+            System.out.println("      DataType=\"http://www.w3.org/2001/XMLSchema#boolean\"");
+        }
+        System.out.println("      MustBePresent=\"true\">");
+        System.out.println("    </AttributeDesignator>");
+        System.out.println("  </Apply>");
+        System.out.println("  <AttributeValue");
+        if (hasRiskScore) {
+            System.out.println("    DataType=\"http://www.w3.org/2001/XMLSchema#integer\">1..10</AttributeValue>");
+        } else {
+            System.out.println("    DataType=\"http://www.w3.org/2001/XMLSchema#boolean\">true</AttributeValue>");
+        }
+        System.out.println("</Apply>");
+        System.out.println();
     }
 }
